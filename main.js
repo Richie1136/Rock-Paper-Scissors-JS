e522532a25c5c22e9117d8adb064a239236c7b07
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-29 14:18:06 -0400

    Grabbing all of the ids

---
+++ main.js
@@ -0,0 +1,6 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const result = document.getElementById('result')
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
