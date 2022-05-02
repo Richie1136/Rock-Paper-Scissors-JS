4e11c081ac3e980fd1dbf6c5e315223dcb5d7724
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-02 14:23:23 -0400

    UpperCasing the first letter of the different options and wrote a conditional for the different numbers which will be the computers choice

--- main.js
+++ main.js
@@ -4,10 +4,32 @@ const result = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 
 let userChoice;
+let computerPick;
+
+
+
+const generateComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+  if (randomNumber === 1) {
+    computerPick = 'rock'
+  }
+  if (randomNumber === 2) {
+    computerPick = 'scissors'
+  }
+  if (randomNumber === 3) {
+    computerPick = 'paper'
+  }
+  console.log(randomNumber)
+}
+
 
 const userChoices = (event) => {
+
   userChoice = event.target.id
-  userChoiceDisplay.innerHTML = userChoice
+  console.log(userChoice)
+  let result = userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase()
+  userChoiceDisplay.innerHTML = result
+  generateComputerChoice()
 }
 
 
